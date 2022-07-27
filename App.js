53c506c41be3006d12676bbb42d9661d50b3a790
chelo603 <chelo_603@hotmail.com>
2022-07-27 20:22:57 -0300

    Desafio 3

---
+++ App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import Navbar from './components/Navbar';
+import ItemListContainer from './components/ItemListContainer';
+import Home from './components/Home';
+
+
+
+function App() {
+  
+  return (
+    <div >
+       <Navbar />
+       <Home name=''/>
+       <ItemListContainer />       
+       
+       
+    </div>
+  );
+}
+
+export default App;
---
+++ components/Home.js
@@ -0,0 +1,16 @@
+
+
+import './Home.css';
+
+
+
+function Home({name}) {
+  return (
+ 
+    <h2>Bienvenido Pets-Ron {name}</h2> 
+    
+    
+  );
+}
+
+export default Home;
